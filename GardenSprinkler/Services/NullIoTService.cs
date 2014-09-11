c4d01fa511c38b83f5df0c5e9078ff7162938b83
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2014-09-11 17:47:28 +0100

    Added NullIoTService to allow for a un-connected system to help see if the networking is causing issues.

---
+++ GardenSprinkler/Services/NullIoTService.cs
@@ -0,0 +1,23 @@
+﻿using GardenSprinkler.Domain;
+using GardenSprinkler.Interfaces;
+using Microsoft.SPOT;
+
+namespace GardenSprinkler.Services
+{
+    public class NullIoTService : IInternetOfThingsService
+    {
+        public event EventHandler WaterRequested;
+
+        public void Start()
+        { }
+
+        public void Stop()
+        { }
+
+        public void PostStatus(string message)
+        { }
+
+        public void PostMeasurements(Measurement currentMeasurement)
+        { }
+    }
+}
